82a7aa3fc9af416fba5be57d2603240cf23b60fe
MatiasNB <bentiez.matiasn@gmail.com>
2025-03-24 22:23:27 -0300

    se modifica codigo para que trabaje sin indice

--- TP1.py
+++ TP1.py
@@ -24,16 +24,18 @@ answers = [
 # Índice de la respuesta correcta para cada pregunta, el el mismo orden que las preguntas
 correct_answers_index = [1, 2, 0, 3, 1]
 
-user_point = 0;
+user_point = 0
+#se genera una lista k-tupla - con ramdon se genera de forma aleatoria
+questions_to_ask = random.choices(list(zip(questions,answers, correct_answers_index)), k=3)
 # El usuario deberá contestar 3 preguntas
-for _ in range(3):
-    # Se selecciona una pregunta aleatoria
-    question_index = random.randint(0, len(questions)-1)
+for questions in questions_to_ask:
     
     # Se muestra la pregunta y las respuestas posibles
-    print(questions[question_index])
-    for i, answer in enumerate(answers[question_index]):
-        print(f"{i + 1}. {answer}")
+    print(questions[0])
+    i=0
+    for answer in questions[1]:
+        print(f"{i+1}. {answer}")
+        i+=1
     # El usuario tiene 2 intentos para responder correctamente
     for intento in range(2):
         user_answer = input("Repuesta: ")
@@ -49,7 +51,7 @@ for _ in range(3):
             user_answer = int(user_answer)-1
             #break
         # Se verifica si la respuesta es correcta
-        if user_answer == correct_answers_index[question_index]:
+        if user_answer == questions[2]:
             print("¡Correcto!")
             user_point+=1
             break 
@@ -57,11 +59,10 @@ for _ in range(3):
         # Si el usuario no responde correctamente después de 2 intentos,
         # se muestra la respuesta correcta
             print("Incorrecto. La respuesta correcta es:")
-            print(answers[question_index]
-    [correct_answers_index[question_index]])
+            print(questions[1][questions[2]])
             user_point-=0.5
  
     # Se imprime un blanco al final de la pregunta
     print()
 #al final imprime el puntaje del usuario    
-print(f'El puntaje del usuario: {user_point}');
+print(f'El puntaje del usuario: {user_point}')
